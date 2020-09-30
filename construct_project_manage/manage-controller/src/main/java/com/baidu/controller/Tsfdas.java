437c59b70bdbb036c5e7219a3641115b4cbe8345
acalculia <2397070892@qq.com>
2020-09-30 10:59:11 +0800

    项目搭建完成05

--- construct_project_manage/manage-controller/src/main/java/com/baidu/controller/Tsfdas.java
+++ construct_project_manage/manage-controller/src/main/java/com/baidu/controller/Tsfdas.java
@@ -1,14 +1,32 @@
 package com.baidu.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class Tsfdas {
 
-
+    @ResponseBody
     @RequestMapping("Demo01")
     public String test01(){
         return "sdfadffsadf";
     }
+
+
+    @RequestMapping("login")
+    public String login(){
+        return "login";
+    }
+
+
+    @RequestMapping("index")
+    public String index(){
+        return "index";
+    }
+
+
+
+
 }
